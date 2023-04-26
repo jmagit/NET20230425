dc81d8b90ce5225d23595b4d439bf301f63456f0
Javier <jmartin@grupoloyal.com>
2023-04-26 13:09:54 +0200

    Ejemplos sintaxis

---
+++ Demos/Calculadora.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos {
+    /// <summary>
+    /// Demo de la documentación
+    /// </summary>
+#if DEBUG
+    public class Calculadora {
+#else
+    internal class Calculadora {
+#endif
+        #region Atributos
+        #endregion
+        #region Propiedades
+        #endregion
+        #region Métodos
+        #region Inicializacion
+        #endregion
+        #region Métodos de calculo
+
+        /// <summary>
+        /// Suma dos valores
+        /// <code>
+        ///  c = suma(2, 3)
+        /// </code>
+        /// </summary>
+        /// <param name="a">Primer operando</param>
+        /// <param name="b">Segundo operando</param>
+        /// <returns>Suma de los dos operandos</returns>
+        public double Suma(double a, double b) {
+#if DEBUG
+            Console.WriteLine(a + b);
+#endif
+            return a + b;
+        }
+        public double Suma(int a, int b, int c) {
+#if DEBUG
+            Console.WriteLine($"int: {a + b}");
+#endif
+            return a + b;
+        }
+        public decimal Suma(decimal a, decimal b) {
+#if DEBUG
+            Console.WriteLine(a + b);
+#endif
+            return a + b;
+        }
+        #endregion
+        #endregion
+    }
+}
--- Demos/Program.cs
+++ Demos/Program.cs
@@ -1,7 +1,51 @@
 ﻿namespace Demos {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Adios mundo");
+            var c = new Calculadora();
+            var i = c.Suma((decimal)0.1, (decimal)0.2);
+            var k = c.Suma(0.1M, 0.2m);
+            var j = c.Suma(1, 3, 3);
+            char letra = '0';
+            Object o = i; // new Decimal(i)
+            //o = c;
+            if(o is Calculadora) {
+                (o as Calculadora).Suma(1,2,3);
+                ((Calculadora)o).Suma(1, 2, 3);
+            }
+            if(c != null && c.Suma(2m, 2m) > 3) {
+
+            }
+            int? edad = null;
+            Nullable<int> n = null; 
+            edad = 5;
+            int años = (int)i;
+            if(edad.HasValue) {
+                años = años + edad.Value;
+            }
+            i = (Decimal)o + 1; // o.value
+            letra++;
+            j = letra;
+            años = 2;
+            int div = años = 2;
+            double o1, o2;
+            if((o1 = c.Suma(1, 2, 3)) > 2 & (o2 = c.Suma(1, 2, 3)) < 2) { }
+            Console.WriteLine($"Divide: {(decimal)div / años}\n");
+            Console.WriteLine($"char: {letra} {j} {'A' + 1}\n");
+            var nombre = "mundo";
+            Console.WriteLine("Adios \"" + nombre + "\"\n");
+            Console.WriteLine($"Adios {nombre}\n");
+            Console.WriteLine(@"c:\demo\curso\nada");
+            if((años += 2) == 2) { }
+            bool b = false;
+            if(!b) { }
+            //o.p.m();
+            //if(o != null && o.p != null && o.p.m() > 0) { }
+            //if(o?.p?.m() > 0) { }
+            //var rslt = c?.Suma(1, 2, 3) ?? 0; //o?.p?.m();
+            //string cad;
+            //cad = (cad??"").ToLower();
+
+
         }
     }
 }
