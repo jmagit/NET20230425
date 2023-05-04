3c1df4da65118431a80066a179533cd0c0fe9d6d
Javier <jmartin@grupoloyal.com>
2023-05-04 13:16:54 +0200

    Temporizadores y colas

--- Funciones/PerticionWeb.cs
+++ Funciones/PerticionWeb.cs
@@ -23,7 +23,8 @@ namespace Funciones {
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req) {
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req
+            ) {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
