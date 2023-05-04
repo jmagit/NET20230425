00e929472f5d262ec4e901fb1e695c1350c619de
Javier <jmartin@grupoloyal.com>
2023-05-04 12:49:38 +0200

    Ejemplos

--- Funciones/NuevoBLOB.cs
+++ Funciones/NuevoBLOB.cs
@@ -14,5 +14,22 @@ namespace Funciones
             Console.WriteLine($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
+
+        [FunctionName("TimerTrigger")]
+        [return: Queue("myqueue-items")]
+        public static string RunTimerTrigger([TimerTrigger("*/15 * * * * *")] TimerInfo myTimer, ILogger log) {
+            if(myTimer.IsPastDue) {
+                log.LogInformation("Timer is running late!");
+            }
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            return $"C# Timer trigger function executed at: {DateTime.Now}";
+        }
+
+        [FunctionName("QueueTrigger")]
+        public static void QueueTrigger([QueueTrigger("myqueue-items")] string myQueueItem, ILogger log) {
+            log.LogInformation($"C# function processed: {myQueueItem}");
+            Console.WriteLine(myQueueItem);
+        }
+
     }
 }
